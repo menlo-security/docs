127d5cd38f6c4e7a4f70545c2f5efda2c8de9ff4
John Paz <john.paz@menlosecurity.com>
2021-08-09 21:12:31 -0400

    Adding scripts.

---
+++ scripts/delete.py
@@ -0,0 +1,13 @@
+import os, glob
+
+
+try:
+    os.rmdir('../7255-Documentation_Hub-html5/out')
+    os.rmdir('../7255-Documentation_Hub-html5')
+    os.remove('../7255-Documentation_Hub-html5_-Upload_to_GitHub.zip')
+    print('Deleted unneeded sub-directories...')
+    
+except:
+   print('[ERROR!] Unable to delete files.')
+
+# Version 1 of unzip-util, see https://github.com/johnapaz/unzip-util for details.
