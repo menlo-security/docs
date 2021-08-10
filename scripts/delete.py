4001cb4a2f2a8a7a671d00be19a49604eb6dd2f8
John Paz <john.paz@menlosecurity.com>
2021-08-10 11:49:47 -0400

    Fixing file paths.

--- scripts/delete.py
+++ scripts/delete.py
@@ -2,9 +2,9 @@ import os, glob
 
 
 try:
-    os.rmdir('../7255-Documentation_Hub-html5/out')
-    os.rmdir('../7255-Documentation_Hub-html5')
-    os.remove('../7255-Documentation_Hub-html5_-Upload_to_GitHub.zip')
+    os.rmdir('./7255-Documentation_Hub-html5/out')
+    os.rmdir('./7255-Documentation_Hub-html5')
+    os.remove('./7255-Documentation_Hub-html5_-Upload_to_GitHub.zip')
     print('Deleted unneeded sub-directories...')
     
 except:
--- scripts/unzip.py
+++ scripts/unzip.py
@@ -7,7 +7,7 @@ def main():
     
     print('Unzipped files...')
     # Create a ZipFile Object and load sample.zip in it
-    with ZipFile('../7255-Documentation_Hub-html5_-Upload_to_GitHub.zip', 'r') as zipObj:
+    with ZipFile('7255-Documentation_Hub-html5_-Upload_to_GitHub.zip', 'r') as zipObj:
        # Extract all the contents of zip file in current directory
        zipObj.extractall()
 
